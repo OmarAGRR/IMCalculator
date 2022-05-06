9f421f397e2b976d1630c3f87c5d95f4cd51c979
eliotvonecklie <eliotvonecklie@gmail.com>
2022-05-05 19:21:17 -0500

    Update favicon

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="favicon.png">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
