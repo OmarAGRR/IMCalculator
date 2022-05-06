a7d81bb4486db3d460237fe302f68adbedabdbcb
eliotvonecklie <eliotvonecklie@gmail.com>
2022-05-05 19:15:25 -0500

    Finish cleanup

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-  header('Content-Type: text/html; charset=UTF-8')
-?>
+<!DOCTYPE html>
 <html>
   <head>
     <title>IMCalculator</title>
@@ -30,9 +28,13 @@
         padding: 8px;
       }
 
-      #customers tr:nth-child(even){background-color: #f2f2f2;}
+      #customers tr:nth-child(even) { 
+        background-color: #f2f2f2;
+      }
 
-      #customers tr:hover {background-color: #ddd;}
+      #customers tr:hover { 
+        background-color: #ddd;
+      }
 
       #customers th {
         padding-top: 12px;
@@ -41,82 +43,114 @@
         background-color: #04AA6D;
         color: white;
       }
-    </style>
-  </head>
-<body>
-<script src="bootstrap&dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" />
-  <h1 style="text-align:center;">Calculadora IMC</h1>
 
-  <div class="col-md-4">
-    <form method="POST" action="#">
-      <div class="form-group">
-        <label for="doc">Altura(M)</label>
-        <input type="number" name="altura" step="any" class="form-control" id="doc">
-      </div>
-    
-      <div class="form-group">
-        <label for="nombre">Masa(KG)</label>
-        <input type="text" name="masa" class="form-control" id="nombre" >
-      </div>
+      .form-group {
+        text-align: left;
+      }
 
-      <input type="submit" value="Calcular" class="btn btn-info" name="btn_calcular">
-    </form>
-    <?php
-      $alt = "";
-      $masa = "";
-      $result = "";    
-    
-      if(isset($_POST['btn_calcular']))
-      {
-        $alt =  $_POST['altura'];
-        $masa =  $_POST['masa'];
-        $alt2 = $alt*$alt;
-        $result = $masa/$alt2;
-        echo "Altura (M): ", $alt;
-        ?> <br> <?php
-        echo "Masa (KG): ", $masa;
-        ?> </h4> <h3> <?php
-        echo "IMC: " , $result;
-        if ($result <= 18.5){
-        ?> <br> <h1 style="color:#FF0000"> <?php echo"Peso inferior al normal";?></h1><?php
-        } elseif($result > 18.5 && $result <= 24.9){
-              ?> <br> <h1 style="color:#32FF00"> <?php echo "Normal";?></h1><?php
-        } elseif($result > 24.9 && $result <= 29.9){
-              ?> <br> <h1 style="color:#FF4D00"> <?php echo "Peso superior al normal";?></h1><?php
-        } elseif($result > 29.9){
-              ?> <br> <h1 style="color:#FF0000"> <?php echo "Obesidad";?> </h1><?php
-              ?> <p>Le recomendamos los siguientes videos para poder disminuir su IMC</p> 
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/bVZsdMrOI30?start=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> <?php
+      .main-div {
+        padding-right: 15px;
+        padding-left: 15px;
+        margin: 0 auto;
+      }
+
+      @media (min-width: 992px) {
+        .main-div {
+          padding: 0;
+          width: 33.33333333%;
         }
-            
+      }
+    </style>
 
-      }  
-      ?>
-      
-      <table id="customers">
-        <tr>
-          <th>Composición corporal</th>
-          <th>Índice de masa corporal (IMC)</th>
-        </tr>
-        <tr>
-          <td>Peso inferior al normal</td>
-          <td>Menos de 18.5</td>
-        </tr>
-        <tr>
-          <td>Normal</td>
-          <td>18.5 – 24.9</td>
-        </tr>
-        <tr>
-          <td>Peso superior al normal</td>
-          <td>25.0 – 29.9</td>
-        </tr>
-        <tr>
-          <td>Obesidad</td>
-          <td>Más de 30.0</td>
-        </tr>
-      </table>   
+    <script src="bootstrap&dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div style="text-align: center;">
+      <h1>Calculadora IMC</h1>
+
+      <div class="main-div">
+        <form method="POST" action="#" style="padding-bottom: 15px;">
+          <div class="form-group">
+            <label for="doc">Altura (m)</label>
+            <input type="number" name="altura" step="0.01" max="3" min="0.5" class="form-control" id="doc" required>
+          </div>
+        
+          <div class="form-group">
+            <label for="nombre">Masa (Kg)</label>
+            <input type="number" name="masa" step="1" class="form-control" id="nombre" required>
+          </div>
+
+          <input type="submit" value="Calcular" class="btn btn-info" name="btn_calcular">
+        </form>
+
+        <?php
+          if(isset($_POST['btn_calcular']) && isset($_POST['altura']) && isset($_POST['masa']))
+          {
+            $alt = $_POST['altura'];
+            $masa = $_POST['masa'];
+
+            $imc = $masa / pow($alt, 2);
+
+            echo <<<EOF
+            <p style="font-size: 1.2em;">
+              Altura (M): $alt
+              <br>
+              Masa (KG): $masa
+            </p>
+            <p style="font-size: 1.8em;">IMC: $imc</p>
+            EOF;
+            
+            if ($imc <= 18.5)
+            {
+              echo "<p style=\"font-size: 2.5em; color: #FF0000;\">Peso inferior al normal</p>";
+            } 
+            else if ($imc > 18.5 && $imc <= 24.9)
+            {
+              echo "<p style=\"font-size: 2.5em; color: #32FF00;\">Normal</p>";
+            } 
+            else if ($imc > 24.9 && $imc <= 29.9)
+            {
+              echo "<p style=\"font-size: 2.5em; color: #FF4D00;\">Peso superior al normal</p>";
+            } 
+            else if ($imc > 29.9)
+            {
+              echo <<<EOF
+              <p style="font-size: 2.5em; color:#FF0000">Obesidad</p>
+              <div>
+                <p>Le recomendamos los siguientes videos para poder disminuir su IMC</p> 
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/bVZsdMrOI30" allow="autoplay; picture-in-picture" allowfullscreen />
+              </div>
+              EOF;
+            }
+          }  
+        ?>
+        
+        <hr>
+
+        <table id="customers">
+          <tr>
+            <th>Composición corporal</th>
+            <th>Índice de masa corporal (IMC)</th>
+          </tr>
+          <tr>
+            <td>Peso inferior al normal</td>
+            <td>Menos de 18.5</td>
+          </tr>
+          <tr>
+            <td>Normal</td>
+            <td>18.5 – 24.9</td>
+          </tr>
+          <tr>
+            <td>Peso superior al normal</td>
+            <td>25.0 – 29.9</td>
+          </tr>
+          <tr>
+            <td>Obesidad</td>
+            <td>Más de 30.0</td>
+          </tr>
+        </table>    
       </div>
-  </div>
+    </div>
   </body>
 </html>
         
